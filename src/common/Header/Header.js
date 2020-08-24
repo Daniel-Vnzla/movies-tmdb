937aabcfcd121c5566936c1006b91d44b496272c
unknown <rune.escape888@gmail.com>
2020-08-24 17:40:57 -0400

    header

---
+++ src/common/Header/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="title">MoV</h1>
+      <div className="navbar-wrapper">
+        <nav className="navbar">
+          <Link className="nav-item" to="/movies">
+            Movies
+          </Link>
+          <span className="separator"> | </span>
+          <Link className="nav-item" to="/tv-shows">
+            Tv Shows
+          </Link>
+        </nav>
+        <div className="search">
+          <input type="text" placeholder="Search..." />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
